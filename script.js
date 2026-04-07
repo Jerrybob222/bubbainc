991cfd570daa12d5141ac725fc785add3bd593fc
tacomeuncher <isaacmcall9@gmail.com>
2026-04-07 11:35:34 -0700

    added inf1n1te cr@ft

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ kid3: { url: "PC/kidgarden3", text: "kidnergarten part 3" },
     rtsr: { url: "ARC/rooftopmath.html", text: "roof sn1p3r r@nd0ms. with b0t or pe0p1e" },
     escr: { url: "ARC/escapeturnpike.html", text: "run away from the police" },
     sm2: { url: "ARC/sm99.html", text: " big apple" },
-
+    inf: { url: "ARC/infc.html", text: " cr@ft inf1n1tly" },
 
 
 
